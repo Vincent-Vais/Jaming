43e5897ae8e3450955d893737d6a88aa59b34021
Vincent-Vais <vincentvais17@gmail.com>
2020-09-14 20:50:51 -0500

    ready to deploy

---
+++ build/precache-manifest.ba660fa3742fcf1886fb18fce5a9786b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3459d281fb6ea197ae9ac93c783d42cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581453b831d44ccbeeb2",
+    "url": "/static/css/main.89392b2f.chunk.css"
+  },
+  {
+    "revision": "5a7f9dfb6e0b3c4c3174",
+    "url": "/static/js/2.43fe0b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43fe0b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581453b831d44ccbeeb2",
+    "url": "/static/js/main.912bc687.chunk.js"
+  },
+  {
+    "revision": "120373d10e26e9062395",
+    "url": "/static/js/runtime-main.5f82ff70.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
